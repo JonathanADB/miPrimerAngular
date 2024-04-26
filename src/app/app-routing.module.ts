1a6efb1462f7633933248378f4e0215b16b1916d
JonathanADB <jhonnysoy1@gmail.com>
2024-04-26 12:52:33 +0200

    funcionan iconos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './views/about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about-me',
+    component: AboutMeComponent,
+  },
+  { path: 'articles', loadChildren: () => import('./views/articles/articles.module').then(m => m.ArticlesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    AboutMeComponent
+    AboutMeComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
