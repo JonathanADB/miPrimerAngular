f1f8e68a0ca8486740b9202914780440760bfc37
JonathanADB <jhonnysoy1@gmail.com>
2024-04-26 11:19:04 +0200

    header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = '';
-  subtitle= ''
+  title = 'Hola Mundo';
+  subtitle = '¿Qué tal?';
 }
